7b562492957361df54917db873a800ff6a209ee9
Hongsblash <skdxxb@outlook.com>
2023-03-17 11:04:46 +1000

    Modified ParentComponent to share current state with child components

--- src/components/ParentComponent.jsx
+++ src/components/ParentComponent.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import WelcomeMessage from './WelcomeMessage';
 import About from './About';
+import TaxationAndAccounting from './TaxationAndAccounting';
+import BusinessAdvisory from './BusinessAdvisory';
+import Innovation from './Innovation';
+import FinancialPlanning from './FinancialPlanning';
+import ContactUs from './ContactUs';
 
 const ParentComponent = () => {
   const [currentView, setCurrentView] = useState('Home');
@@ -9,15 +14,28 @@ const ParentComponent = () => {
     setCurrentView(view);
   };
 
-  return (
-    <div>
-      {currentView === 'Home' ? (
-        <WelcomeMessage changeView={changeView} />
-      ) : (
-        <About changeView={changeView} />
-      )}
-    </div>
-  );
+  const renderView = () => {
+    switch (currentView) {
+      case 'Home':
+        return <WelcomeMessage changeView={changeView} />;
+      case 'About':
+        return <About changeView={changeView} />;
+      case 'taxationAndAccounting':
+        return <TaxationAndAccounting changeView={changeView} />;
+      case 'businessAdvisory':
+        return <BusinessAdvisory changeView={changeView} />;
+      case 'innovation':
+        return <Innovation changeView={changeView} />;
+      case 'financialPlanning':
+        return <FinancialPlanning changeView={changeView} />;
+      case 'ContactUs':
+        return <ContactUs changeView={changeView} />;
+      default:
+        return null;
+    }
+  };
+
+  return <div>{renderView()}</div>;
 };
 
 export default ParentComponent;
